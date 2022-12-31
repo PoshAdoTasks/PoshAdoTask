e1ed81dea2d34c16121a6b32b7ddfae0c5ad3463
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-31 01:09:48 -0600

    Added NewContent

---
+++ PoshAdoTask/NewContent.cs
@@ -0,0 +1,36 @@
+﻿namespace PoshAdoTask.Cmdlets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Management.Automation;
+    using PoshAdoTask.Types;
+
+    [Cmdlet(VerbsCommon.New, "Content", HelpUri = "")]
+    [OutputType(typeof(PoshAdoTask.Types.Content))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class NewContent : PSCmdlet
+    {
+        [Parameter(Mandatory = false, Position = 0)]
+        public string Details { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 1)]
+        public string License { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 2)]
+        public string Pricing { get; set; } = string.Empty;
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("NewContent : Begin Processing");
+            WriteVerbose("Details    : " + Details);
+            WriteVerbose("License    : " + License);
+            WriteVerbose("Pricing    : " + Pricing);
+
+            Content newContent = new();
+            if (!(string.IsNullOrEmpty(Details))) { newContent.Details = new ContentDefinition(Details); }
+            if (!(string.IsNullOrEmpty(License))) { newContent.License = new ContentDefinition(License); }
+            if (!(string.IsNullOrEmpty(Pricing))) { newContent.Pricing = new ContentDefinition(Pricing); }
+            
+            WriteObject(newContent);
+        }
+    }
+}
