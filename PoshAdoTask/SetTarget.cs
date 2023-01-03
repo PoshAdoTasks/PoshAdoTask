4ab2a606fcc1799112de883066a2636d5e1efb7d
Jeffrey Patton <jeffrey@patton-tech.com>
2023-01-03 12:39:02 -0600

    SetTarget Logic working

---
+++ PoshAdoTask/SetTarget.cs
@@ -0,0 +1,69 @@
+﻿namespace PoshAdoTask.Cmdlets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Management.Automation;
+    using PoshAdoTask.Types;
+    using System.Configuration;
+
+    [Cmdlet(VerbsCommon.Set, "Target", HelpUri = "")]
+    [OutputType(typeof(PoshAdoTask.Types.Link))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class SetTarget : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public Manifest? Manifest { get; set; }
+        [Parameter(Mandatory = true, Position = 1)]
+        [ValidateSet("Microsoft.TeamFoundation.Server", "Microsoft.TeamFoundation.Server.Integration", "Microsoft.VisualStudio.Services", "Microsoft.VisualStudio.Services.Cloud", "Microsoft.VisualStudio.Services.Cloud.Integration", "Microsoft.VisualStudio.Services.Integration")]
+        public string? TargetId { get; set; }
+        [Parameter(Mandatory = false, Position = 2)]
+        public string? Version { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 3)]
+        public SwitchParameter Remove { get; set; }
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("SetTarget : Begin Processing");
+            WriteVerbose("TargetId  : " + TargetId);
+            WriteVerbose("Version   : " + Version);
+            WriteVerbose("Remove    : " + Remove);
+        }
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("SetTarget : Process Record");
+            if (Manifest != null)
+            {
+                Target newTarget = new();
+                if (!(string.IsNullOrEmpty(TargetId)))
+                {
+                    newTarget = new(TargetId);
+                    WriteVerbose("Target created id");
+                }
+                if (!(string.IsNullOrEmpty(TargetId)) && !(string.IsNullOrEmpty(Version)))
+                {
+                    newTarget = new(TargetId, Version);
+                    WriteVerbose("Target created id, version");
+                }
+                
+                bool Found = Manifest.Targets.Exists(t => t.Id == TargetId);
+                WriteVerbose("Found Target: " + Found);
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(Remove)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[(nameof(Remove))] && Found)
+                    {
+                        WriteVerbose("Remove Target");
+                    }
+                } else  if (!(Found))
+                {
+                    WriteVerbose("Add Target");
+                }
+            }
+        }
+        protected override void EndProcessing()
+        {
+            WriteVerbose("SetTarget : End Process");
+            WriteObject(Manifest);
+        }
+    }
+}
