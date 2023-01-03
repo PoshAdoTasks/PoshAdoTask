76481abfa4c62a62474d9b04dd8b9655bb8ddada
Jeffrey Patton <jeffrey@patton-tech.com>
2023-01-03 16:13:58 -0600

    Added setgallery cmdlet, as that object is not set at creation, use null coalescense to create the list

---
+++ PoshAdoTask/SetGalleryFlag.cs
@@ -0,0 +1,118 @@
+﻿namespace PoshAdoTask.Cmdlets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Management.Automation;
+    using PoshAdoTask.Types;
+    using System.Configuration;
+    using System.Management;
+
+    [Cmdlet(VerbsCommon.Set, "GalleryFlag", HelpUri = "")]
+    [OutputType(typeof(PoshAdoTask.Types.Link))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class SetGalleryFlag : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public Manifest? Manifest { get; set; }
+        [Parameter(Mandatory = false, Position = 1)]
+        public SwitchParameter Paid { get; set; }
+        [Parameter(Mandatory = false, Position = 2)]
+        public SwitchParameter Preview { get; set; }
+        [Parameter(Mandatory = false, Position = 3)]
+        public SwitchParameter Public { get; set; }
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("SetGalleryFlag : Begin Processing");
+            WriteVerbose("Paid           : " + Paid);
+            WriteVerbose("Preview        : " + Preview);
+            WriteVerbose("Public         : " + Public);
+        }
+        protected override void ProcessRecord()
+        {
+            if (Manifest != null)
+            {
+                int Capacity = (Manifest.GalleryFlags ??= new List<GalleryFlag>()).Capacity;
+            }
+            WriteVerbose("SetGalleryFlag : Process Record");
+            if (Manifest != null && Manifest.GalleryFlags != null)
+            {
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(Paid)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(Paid)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.GalleryFlags.Contains(GalleryFlag.Paid)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.GalleryFlags.Add(GalleryFlag.Paid);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.GalleryFlags.Contains(GalleryFlag.Paid))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.GalleryFlags.Remove(GalleryFlag.Paid);
+                        }
+                    }
+                }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(Preview)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(Preview)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.GalleryFlags.Contains(GalleryFlag.Preview)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.GalleryFlags.Add(GalleryFlag.Preview);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.GalleryFlags.Contains(GalleryFlag.Preview))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.GalleryFlags.Remove(GalleryFlag.Preview);
+                        }
+                    }
+                }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(Public)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(Public)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.GalleryFlags.Contains(GalleryFlag.Public)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.GalleryFlags.Add(GalleryFlag.Public);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.GalleryFlags.Contains(GalleryFlag.Public))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.GalleryFlags.Remove(GalleryFlag.Public);
+                        }
+                    }
+                }
+            }
+        }
+        protected override void EndProcessing()
+        {
+            WriteVerbose("SetGalleryFlag : End Process");
+            WriteObject(Manifest);
+        }
+    }
+}
