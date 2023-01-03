b57fb7adab3d1ecd2bb74434b98f3a83e9da508e
Jeffrey Patton <jeffrey@patton-tech.com>
2023-01-03 10:57:48 -0600

    Working SetCategory -switch | -switch:$true adds, -switch:$false removes

--- PoshAdoTask/SetCategory.cs
+++ PoshAdoTask/SetCategory.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using System.Management.Automation;
     using PoshAdoTask.Types;
+    using System.Configuration;
 
     [Cmdlet(VerbsCommon.Set, "Category", HelpUri = "")]
     [OutputType(typeof(PoshAdoTask.Types.Link))]
@@ -39,17 +40,120 @@
             WriteVerbose("SetCategory    : Process Record");
             if (Manifest != null)
             {
-                // need to test if the flag is set
-                // need to decide if we're removing
-                // need to decide if we're setting
-                // can we test for -AzureArtifacts:$true -AzureArtifacts:$false
-                WriteVerbose(MyInvocation.BoundParameters.ContainsKey(nameof(AzureArtifacts)).ToString());
-                WriteVerbose(MyInvocation.BoundParameters[nameof(AzureArtifacts)].ToString());
-                if (AzureArtifacts) { Manifest.Categories.Add(ManifestCategory.AzureArtifacts); }
-                if (AzureBoards) { Manifest.Categories.Add(ManifestCategory.AzureBoards); }
-                if (AzurePipelines) { Manifest.Categories.Add(ManifestCategory.AzurePipelines); }
-                if (AzureRepos) { Manifest.Categories.Add(ManifestCategory.AzureRepos); }
-                if (AzureTestPlans) { Manifest.Categories.Add(ManifestCategory.AzureTestPlans); }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(AzureArtifacts)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(AzureArtifacts)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.Categories.Contains(ManifestCategory.AzureArtifacts)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.Categories.Add(ManifestCategory.AzureArtifacts);
+                        }
+                    } else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.Categories.Contains(ManifestCategory.AzureArtifacts))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.Categories.Remove(ManifestCategory.AzureArtifacts);
+                        }
+                    }
+                }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(AzureBoards)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(AzureBoards)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.Categories.Contains(ManifestCategory.AzureBoards)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.Categories.Add(ManifestCategory.AzureBoards);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.Categories.Contains(ManifestCategory.AzureBoards))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.Categories.Remove(ManifestCategory.AzureBoards);
+                        }
+                    }
+                }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(AzurePipelines)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(AzurePipelines)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.Categories.Contains(ManifestCategory.AzurePipelines)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.Categories.Add(ManifestCategory.AzurePipelines);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.Categories.Contains(ManifestCategory.AzurePipelines))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.Categories.Remove(ManifestCategory.AzurePipelines);
+                        }
+                    }
+                }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(AzureRepos)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(AzureRepos)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.Categories.Contains(ManifestCategory.AzureRepos)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.Categories.Add(ManifestCategory.AzureRepos);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.Categories.Contains(ManifestCategory.AzureRepos))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.Categories.Remove(ManifestCategory.AzureRepos);
+                        }
+                    }
+                }
+                if (MyInvocation.BoundParameters.ContainsKey(nameof(AzureTestPlans)))
+                {
+                    if ((SwitchParameter)MyInvocation.BoundParameters[nameof(AzureTestPlans)])
+                    {
+                        WriteVerbose("Flag should be added");
+                        WriteVerbose("Test is flag exists");
+                        if (!(Manifest.Categories.Contains(ManifestCategory.AzureTestPlans)))
+                        {
+                            WriteVerbose("Add Flag");
+                            Manifest.Categories.Add(ManifestCategory.AzureTestPlans);
+                        }
+                    }
+                    else
+                    {
+                        WriteVerbose("Flag should be removed");
+                        WriteVerbose("Test is flag exists");
+                        if (Manifest.Categories.Contains(ManifestCategory.AzureTestPlans))
+                        {
+                            WriteVerbose("Remove Flag");
+                            Manifest.Categories.Remove(ManifestCategory.AzureTestPlans);
+                        }
+                    }
+                }
             }
         }
         protected override void EndProcessing()
